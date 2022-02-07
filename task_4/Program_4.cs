bac1bd2ddc41413b5aa90e231424322af07e120c
dr_shov <dr_shov@rambler.ru>
2022-02-07 11:15:16 +0300

    program_4 code is complited

---
+++ task_4/Program_4.cs
@@ -0,0 +1,18 @@
+﻿// Показать чётные числа от 1 до N
+
+Console.WriteLine("Программа показывает четные числа от 1 до N. Введите желаемое число N: ");
+
+string str1 = Console.ReadLine();
+
+int n = Convert.ToInt32(str1);
+
+int a = 2;
+
+while (n > a)
+{
+    Console.Write(a);
+    Console.Write(" ");
+    a = a + 2;
+
+}
+
