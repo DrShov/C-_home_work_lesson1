91f6550e9bd6c54bb56a39d7bebd96fae29bbfe8
dr_shov <dr_shov@rambler.ru>
2022-02-07 11:12:24 +0300

    program_3 code is complited

---
+++ task_3/Program_3.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число для определения его четности: ");
+
+string str1 = Console.ReadLine();
+
+int a = Convert.ToInt32(str1);
+
+int b = a / 2;
+
+if (a - b * 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+
+else
+{
+    Console.WriteLine("Число нечетное");
+}
